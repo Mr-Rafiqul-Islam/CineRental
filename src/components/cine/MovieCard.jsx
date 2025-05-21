76594a9d064ae1d7a04085abf4841d8f61f736db
Mr. Rafiqul Islam <rafiqulislamemon05@gmail.com>
2025-05-21 21:05:32 +0600

    Rating & MovieCard final design

--- src/components/cine/MovieCard.jsx
+++ src/components/cine/MovieCard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { getImgUrl } from "../../utils/cine-utils";
-import star from "../../assets/star.svg"
+import { getImgUrl } from "../../utils/cine-utils";mO
+import Rating from "./Rating";
 
 export default function MovieCard({movie}) {
   return (
@@ -10,11 +10,7 @@ export default function MovieCard({movie}) {
         <h3 className="text-xl mb-1">{movie.title}</h3>
         <p className="text-[#575A6E] text-sm mb-2">{movie.genre}</p>
         <div className="flex items-center space-x-1 mb-5">
-          <img src={star} width={14} height={14} alt="star" />
-          <img src={star} width={14} height={14} alt="star" />
-          <img src={star} width={14} height={14} alt="star" />
-          <img src={star} width={14} height={14} alt="star" />
-          <img src={star} width={14} height={14} alt="star" />
+          <Rating value={movie.rating}/>
         </div>
         <a
           className="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import SideBar from "../components/SideBar";
-import MovieList from "../components/movie/MovieList";
+import MovieList from "../components/cine/MovieList";
 
 export default function Home() {
   return (
