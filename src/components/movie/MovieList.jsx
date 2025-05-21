4881c12734d9170d51a1fe56e93fb7041dc55e52
Mr. Rafiqul Islam <rafiqulislamemon05@gmail.com>
2025-05-21 08:00:07 +0600

    primary all visible layouts done

---
+++ src/components/movie/MovieList.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { getAllMovies } from "../../data/movie";
+import MovieCard from "./MovieCard";
+
+export default function MovieList() {
+    const movies = getAllMovies();
+  return (
+    <div className="content">
+      <div className="grid sm:grid-cols-2 xl:grid-cols-3 gap-7">
+        {
+            movies.map((item)=>(
+                <MovieCard key={item.id} movie={item}/>
+            ))
+        }
+        
+        
+      </div>
+    </div>
+  );
+}
