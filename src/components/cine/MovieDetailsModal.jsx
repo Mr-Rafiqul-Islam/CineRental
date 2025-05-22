6c1d3e5933be367f5e32543ef8ec15c64bd0534a
Mr. Rafiqul Islam <rafiqulislamemon05@gmail.com>
2025-05-22 07:36:00 +0600

    add cart from movie details

--- src/components/cine/MovieDetailsModal.jsx
+++ src/components/cine/MovieDetailsModal.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Tag from "../../assets/tag.svg";
 import { getImgUrl } from "../../utils/cine-utils";
 
-export default function MovieDetailsModal({ movie, onclose }) {
+export default function MovieDetailsModal({ movie, onclose, onCartAdd }) {
   return (
     <div className="fixed top-0 left-0 w-screen h-screen z-50 bg-black/60 backdrop-blur-sm">
       <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-full max-w-[420px] sm:max-w-[600px] lg:max-w-[984px] p-4 max-h-[90vh] overflow-auto">
@@ -29,6 +29,7 @@ export default function MovieDetailsModal({ movie, onclose }) {
               <a
                 className="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
                 href="#"
+                onClick={(e) => onCartAdd(e, movie)}
               >
                 <img src={Tag} alt="Tag" />
                 <span>${movie.price} | Add to Cart</span>
